d46baafeae4d8a512233e011ff24a705de887e12
Sid Jha <sidjha03@gmail.com>
2020-09-06 19:59:38 -0400

    Added profile admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'id']
+    search_fields = ['user__username']
+
+admin.site.register(Profile, ProfileAdmin)
