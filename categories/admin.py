8c41c349988d4c4e22432d375b8392848b8a46c1
Sid Jha <sidjha03@gmail.com>
2020-09-06 19:57:49 -0400

    Added category admin

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'total_videos', 'total_views', 'total_likes']
+    search_fields = ['title']
+
+admin.site.register(Category, CategoryAdmin)
