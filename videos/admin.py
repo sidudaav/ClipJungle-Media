01811a99c52f66377523b5d4ca81d447d2bc222a
Sid Jha <sidjha03@gmail.com>
2020-09-06 20:01:48 -0400

    Added video admin

--- videos/admin.py
+++ videos/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Video
 
-# Register your models here.
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ['profile', 'id', 'title', 'slug', 'count_categories', 'hot_score', 'created_on', 'active']
+    list_filter = ['created_on']
+    search_fields = ['user__username', 'title', 'categories__title']
+    actions = ['approve_videos', 'block_videos']
+
+    def count_categories(self, obj):
+        return obj.categories.count()
+    count_categories.short_description = 'Categories'
+
+    def approve_videos(self, request, queryset):
+        queryset.update(active=True)
+
+    def block_videos(self, request, queryset):
+        queryset.update(active=False) 
+
+admin.site.register(Video, VideoAdmin)
