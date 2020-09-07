284e14e2d7b094f7b43d76362987581f25b10a3c
Sid Jha <sidjha03@gmail.com>
2020-09-07 06:48:59 -0400

    Added block profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, ProfileBlock, ProfileFollowRequest, ProfileFollowing
 from django.contrib.auth.hashers import check_password
 from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.contrib.auth.decorators import login_required
 
+######################## AUUTHENTICATION VIEWS ########################
 def register(request):
     if request.method == 'POST':
         first_name = request.POST.get('firstName')
@@ -26,7 +27,7 @@ def register(request):
         
     if request.user.is_authenticated:
         return redirect("profiles:success-page")
-        
+
     return render(request, 'profiles/register.html')
 
 def login(request):
@@ -66,3 +67,32 @@ def logout(request):
 def success_page(request):
     print(request.user)
     return render(request, 'profiles/success.html')
+
+
+######################## PROFILE INTERACTION VIEWS ########################
+@login_required
+def block_profile(request):
+    profile_id = request.POST.get('id')
+    action = request.POST.get('action')
+
+    if profile_id and action:
+        try:
+            blocked_profile = get_object_or_404(Profile, id=profile_id)
+            if action == 'block':
+                ProfileBlock.objects.get_or_create(profile_from=request.user.profile, profile_to=blocked_profile)
+
+                ProfileFollowRequest.objects.filter(profile_from=request.user.profile, profile_to=blocked_profile).delete()
+                ProfileFollowRequest.objects.filter(profile_from=blocked_profile, profile_to=request.user.profile).delete()
+
+                ProfileFollowing.objects.filter(profile_from=request.user.profile, profile_to=blocked_profile).delete()
+                ProfileFollowing.objects.filter(profile_from=blocked_profile, profile_to=request.user.profile).delete()
+
+            else:
+                ProfileBlock.objects.filter(profile_from=request.user.profile, user_to=blocked_profile)
+            
+            return JsonResponse({ 'status': 'OK' })
+        
+        except:
+            pass
+    
+    return JsonResponse({ 'status': 'KO' })
