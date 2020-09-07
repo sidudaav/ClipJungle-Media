15c4aa114b2988097599af836cea1462329514af
Sid Jha <sidjha03@gmail.com>
2020-09-07 14:42:59 -0400

    Set login url

--- ClipJungle/settings.py
+++ ClipJungle/settings.py
@@ -105,6 +105,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+#############
+LOGIN_URL = "/profiles/login/"
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
