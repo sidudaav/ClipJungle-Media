b5982acb64e221832ea315a09635034a416257d3
Sid Jha <sidjha03@gmail.com>
2020-09-07 03:34:13 -0400

    Fixed base js script tag

--- profiles/static/profiles/js/register.js
+++ profiles/static/profiles/js/register.js
@@ -1,19 +1,3 @@
-var csrftoken = $.cookie('csrftoken');
-function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-
-}
-
-// Sets up AJAX
-$.ajaxSetup({
-    beforeSend: function(xhr, settings) {
-        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-            xhr.setRequestHeader("X-CSRFToken", csrftoken);
-        }
-    }
-});
-
 $(document).ready(() => {
     const DOMStrings = {
         registrationForm : $('.form'),
