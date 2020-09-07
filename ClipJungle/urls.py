0416354efdc42bd0b4a443c3643ecef52f8e5b63
Sid Jha <sidjha03@gmail.com>
2020-09-07 13:01:12 -0400

    Added Video Report Admin and edited Video Admin

--- ClipJungle/urls.py
+++ ClipJungle/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('profiles/', include('profiles.urls', namespace='profiles'))
+    path('profiles/', include('profiles.urls', namespace='profiles')),
 ]
--- videos/admin.py
+++ videos/admin.py
@@ -1,15 +1,11 @@
 from django.contrib import admin
-from .models import Video
+from .models import Video, VideoReport
 
 class VideoAdmin(admin.ModelAdmin):
-    list_display = ['profile', 'id', 'title', 'slug', 'count_categories', 'hot_score', 'created_on', 'active']
+    list_display = ['profile', 'id', 'title', 'slug', 'category', 'hot_score', 'created_on', 'active']
     list_filter = ['created_on']
-    search_fields = ['user__username', 'title', 'categories__title']
+    search_fields = ['profile', 'title', 'category__title']
     actions = ['approve_videos', 'block_videos']
-
-    def count_categories(self, obj):
-        return obj.categories.count()
-    count_categories.short_description = 'Categories'
 
     def approve_videos(self, request, queryset):
         queryset.update(active=True)
@@ -17,4 +13,18 @@ class VideoAdmin(admin.ModelAdmin):
     def block_videos(self, request, queryset):
         queryset.update(active=False) 
 
+class VideoReportAdmin(admin.ModelAdmin):
+    list_display = ['reported_profile', 'reporting_profile', 'created_on', 'choice_reason', 'video_id']
+    list_filter = ['created_on']
+    search_fields = ['video__title', 'written_reason', 'choice_reason']
+
+    def reported_profile(self, obj):
+        return obj.video.profile
+    reported_profile.short_description = 'Profile Reported'
+
+    def reporting_profile(self, obj):
+        return obj.profile
+    reporting_profile.short_description = 'Profile Reporting'
+
 admin.site.register(Video, VideoAdmin)
+admin.site.register(VideoReport, VideoReportAdmin)
