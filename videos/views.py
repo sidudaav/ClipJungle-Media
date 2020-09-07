751e0e70d344abef6e923654538c36c5a3a4de5f
Sid Jha <sidjha03@gmail.com>
2020-09-07 07:16:10 -0400

    Added a like video view

--- videos/views.py
+++ videos/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Video, VideoReport
+from django.http import JsonResponse
 
-# Create your views here.
+######################## JSON RESPONSE VIEWS ########################
+def like_video(request):
+    video_id = request.POST.get('id')
+    action = request.POST.get('action')
+
+    if video_id and action:
+        try:
+            video = get_object_or_404(Video, id=video_id)
+            if action == 'like':
+                video.likes.add(request.user.profile)
+            else:
+                video.likes.remove(request.user.profile)
+
+            return JsonResponse({ 'status': 'OK' })
+        
+        except:
+            pass
+
+    return JsonResponse({ 'status': 'KO'})
