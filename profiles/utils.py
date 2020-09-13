d8fa062170410696d40b0a5317620af9054973d6
Siddharth Jha <sidjha03@gmail.com>
2020-09-12 23:55:10 -0400

    Removed case sensitive from get user by email

--- profiles/utils.py
+++ profiles/utils.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 ######################## BASIC FIND USER FUNCTIONS ########################
 def get_user_by_email(email):
     try:
-        return User.objects.get(email=email)
+        return User.objects.get(email=email.capitalize())
     except User.DoesNotExist:
         return None
 
